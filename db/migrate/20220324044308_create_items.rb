e52dbaf8f448d63f718d8e829b6a6751452a1979
Sherman Au <shermau13@gmail.com>
2022-03-23 22:44:11 -0600

    add/test:#item model

---
+++ db/migrate/20220324044308_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.references :item_shop, foreign_key: true
+      t.string :name
+      t.integer :quantity
+      t.float :price
+      t.boolean :consumable
+
+      t.timestamps
+    end
+  end
+end
