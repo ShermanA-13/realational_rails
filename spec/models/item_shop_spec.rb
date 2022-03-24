a9cde10bf4fa797b029f4bf8c467014b6087cdbd
Sherman Au <shermau13@gmail.com>
2022-03-24 15:08:27 -0600

    test: validates presence of attributes

--- spec/models/item_shop_spec.rb
+++ spec/models/item_shop_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ItemShop, type: :model do
+  describe 'attributes' do
+    it { should be_valid }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :city }
+    it { should allow_value(nil).for(:open) }
+  end
+
   describe 'relationships' do
     it { should have_many :items }
   end
