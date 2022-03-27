9ecdda830a86d0bec2a2a490e34c3eb8cb651ede
Sherman Au <shermau13@gmail.com>
2022-03-27 16:10:04 -0600

    test:validate Items attributes

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to :item_shop }
   end
+
+  describe 'attributes' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :quantity }
+    it { should validate_presence_of :price }
+    it { should allow_value([true, false]).for(:consumable) }
+    it { should allow_value(nil).for(:consumable) }
+  end
 end
