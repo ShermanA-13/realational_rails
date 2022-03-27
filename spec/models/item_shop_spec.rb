ced7dbd57976e9e06ad3429e39a176cac06c754a
Sherman Au <shermau13@gmail.com>
2022-03-27 16:08:33 -0600

    refactor:merge conflicts

--- spec/models/item_shop_spec.rb
+++ spec/models/item_shop_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ItemShop, type: :model do
   describe 'attributes' do
-    it { should be_valid }
     it { should validate_presence_of :name }
     it { should validate_presence_of :city }
-    it { should allow_value(nil).for(:open) }
+    it { should allow_value([true, false]).for(:open) }
+    it { should_not allow_value(nil).for(:open) }
   end
 
   describe 'relationships' do
