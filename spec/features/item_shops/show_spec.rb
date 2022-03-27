4bda2add71f250a2242b44006a6deddaf885648c
Sherman Au <shermau13@gmail.com>
2022-03-27 16:38:04 -0600

    test:#show page for ItemShops

---
+++ spec/features/item_shops/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'ItemShops show page' do
+  before :each do
+    @groggy = ItemShop.create!(
+      name: "Groggy's Potions and Knick Knacks",
+      city: 'Akros',
+      open: true,
+      num_employees: 3
+    )
+    @perisophia = ItemShop.create!(
+      name: "Perisophia's Scrolls",
+      city: 'Meletis',
+      open: false,
+      num_employees: 5
+    )
+  end
+
+  it 'displays specific ItemShop data' do
+    visit "/item_shops/#{@groggy.id}"
+
+    save_and_open_page
+    expect(page).to have_content(groggy.name)
+    expect(page).to have_content(groggy.city)
+    expect(page).to have_content(groggy.open)
+    expect(page).to have_content(groggy.num_employees)
+  end
+end
