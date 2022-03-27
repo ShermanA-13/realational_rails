897391c32afb8548cc62f898d619777d171484c0
Sherman Au <shermau13@gmail.com>
2022-03-27 13:46:40 -0600

    test:validate attributes for item_shop

--- spec/models/item_shop_spec.rb
+++ spec/models/item_shop_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ItemShop, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :city }
+    it { should allow_value([true, false]).for :open }
+    it { should_not allow_value(nil).for :open }
+  end
+
   describe 'relationships' do
     it { should have_many :items }
   end
