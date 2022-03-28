0282cb25ec2668a646468deaf64a2a1d35eb13e9
Sherman Au <shermau13@gmail.com>
2022-03-28 13:26:05 -0600

    refactor test:ItemShop #show displays item count associated with shop

--- spec/features/item_shops/show_spec.rb
+++ spec/features/item_shops/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'ItemShops show page' do
+RSpec.describe 'ItemShops #show page' do
   before :each do
     @groggy = ItemShop.create!(
       name: "Groggy's Potions and Knick Knacks",
@@ -14,18 +14,57 @@ RSpec.describe 'ItemShops show page' do
       open: false,
       num_employees: 5
     )
+    @item = @groggy.items.create!(
+      name: 'Stick',
+      quantity: 26,
+      price: 0.02,
+      consumable: false
+    )
+    @item2 = @groggy.items.create!(
+      name: 'Healing Potion',
+      quantity: 12,
+      price: 50.50,
+      consumable: true
+    )
+    @item3 = @groggy.items.create!(
+      name: 'Greater Healing Potion',
+      quantity: 12,
+      price: 100.23,
+      consumable: true
+    )
+
+    @item4 = @perisophia.items.create!(
+      name: 'Scroll of Minor Illusion',
+      quantity: 6,
+      price: 100.50,
+      consumable: true
+    )
+    @item5 = @perisophia.items.create!(
+      name: 'Scroll of Spiritual Weapon',
+      quantity: 12,
+      price: 335.68,
+      consumable: true
+    )
+    @item6 = @perisophia.items.create!(
+      name: 'Wand of Magic Missles',
+      quantity: 2,
+      price: 1205.35,
+      consumable: false
+    )
   end
 
-  it 'displays specific ItemShop data' do
+  it 'displays groggy ItemShop data' do
     visit "/item_shops/#{@groggy.id}"
 
     expect(page).to have_content(@groggy.name)
     expect(page).to have_content(@groggy.city)
     expect(page).to have_content(@groggy.open)
     expect(page).to have_content(@groggy.num_employees)
+    expect(page).to have_content("Number of Items: #{@groggy.count_items}")
+    expect(page).to_not have_content(@groggy.name)
   end
 
-  it 'displays specific ItemShop data' do
+  it 'displays perisophia ItemShop data' do
     visit "/item_shops/#{@perisophia.id}"
 
     # save_and_open_page
@@ -33,6 +72,7 @@ RSpec.describe 'ItemShops show page' do
     expect(page).to have_content(@perisophia.city)
     expect(page).to have_content(@perisophia.open)
     expect(page).to have_content(@perisophia.num_employees)
+    expect(page).to have_content("Number of Items: #{@perisophia.count_items}")
     expect(page).to_not have_content(@groggy.name)
   end
 end
