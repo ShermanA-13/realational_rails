692587e99c63fd9690f2451fa470b6ff86f0cb25
Sherman Au <shermau13@gmail.com>
2022-03-29 11:57:19 -0600

    refactor:#recent_order ItemShop from view

--- app/controllers/item_shops_controller.rb
+++ app/controllers/item_shops_controller.rb
@@ -1,9 +1,11 @@
 class ItemShopsController < ApplicationController
   def index
-    @item_shops = ItemShop.all
+    @item_shops = ItemShop.recent_order
   end
 
   def show
     @item_shop = ItemShop.find(params[:id])
   end
+
+  def new; end
 end
