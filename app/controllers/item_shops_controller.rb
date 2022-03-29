ae6bedf10c52fc757929e3c7bdda3d9cb0b0fd54
Sherman Au <shermau13@gmail.com>
2022-03-29 15:45:03 -0600

    refactor:strong params for ItemShopController

--- app/controllers/item_shops_controller.rb
+++ app/controllers/item_shops_controller.rb
@@ -10,13 +10,14 @@ class ItemShopsController < ApplicationController
   def new; end
 
   def create
-    item_shop = ItemShop.create!(
-      name: params[:name],
-      city: params[:city],
-      open: params[:open],
-      num_employees: params[:num_employees]
-    )
+    item_shop = ItemShop.create!(item_shop_params)
     item_shop.save
     redirect_to '/item_shops'
+  end
+
+  private
+
+  def item_shop_params
+    params.permit(:name, :city, :open, :num_employees)
   end
 end
