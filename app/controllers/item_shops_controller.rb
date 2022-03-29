a287aadd2246513ed4a0f99cc3cb677ad9cae7ff
Sherman Au <shermau13@gmail.com>
2022-03-29 15:23:02 -0600

    feat:#create ItemShopController

--- app/controllers/item_shops_controller.rb
+++ app/controllers/item_shops_controller.rb
@@ -8,4 +8,15 @@ class ItemShopsController < ApplicationController
   end
 
   def new; end
+
+  def create
+    item_shop = ItemShop.create!(
+      name: params[:name],
+      city: params[:city],
+      open: params[:open],
+      num_employees: params[:num_employees]
+    )
+    item_shop.save
+    redirect_to '/item_shops'
+  end
 end
