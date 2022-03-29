933ed85c56d2c7e8bf0415baf5be578d8f9560d2
Sherman Au <shermau13@gmail.com>
2022-03-29 16:42:30 -0600

    feat:#edit ItemShopController

--- app/controllers/item_shops_controller.rb
+++ app/controllers/item_shops_controller.rb
@@ -15,6 +15,10 @@ class ItemShopsController < ApplicationController
     redirect_to '/item_shops'
   end
 
+  def edit
+    @city = ItemShop.find(params[:id])
+  end
+
   private
 
   def item_shop_params
