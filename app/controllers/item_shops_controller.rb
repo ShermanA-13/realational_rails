d12749553eb7ca89e5cb32924c1e2d378b7de3a2
ShermanA-13 <95321606+ShermanA-13@users.noreply.github.com>
2022-03-29 16:34:32 -0600

    It2 (#7)
    * refactor:attribute headers
    
    * refactor:#recent_order ItemShop from view
    
    * test:item_shop#new
    
    * feat:item_shops#new route
    
    * add:form for item_shop#new
    
    * add:form for item_shop#new
    
    * refactor:#num_employees label output
    
    * add:path post #item_shop_create
    
    * feat:#create ItemShopController

--- app/controllers/item_shops_controller.rb
+++ app/controllers/item_shops_controller.rb
@@ -1,9 +1,22 @@
 class ItemShopsController < ApplicationController
   def index
-    @item_shops = ItemShop.all
+    @item_shops = ItemShop.recent_order
   end
 
   def show
     @item_shop = ItemShop.find(params[:id])
+  end
+
+  def new; end
+
+  def create
+    item_shop = ItemShop.create!(
+      name: params[:name],
+      city: params[:city],
+      open: params[:open],
+      num_employees: params[:num_employees]
+    )
+    item_shop.save
+    redirect_to '/item_shops'
   end
 end
---
+++ spec/features/item_shops/new_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'ItemShop creation' do
+  it 'links to new page from item_shop#index' do
+    visit '/item_shops'
+
+    click_link 'New Item Shop'
+    expect(current_path).to eq('/item_shops/new')
+  end
+
+  it 'can create a new artist' do
+    visit '/item_shops/new'
+
+    fill_in('Name', with: "Boomba's Brews")
+    fill_in('City', with: 'Akros')
+    fill_in('Open', with: true)
+    fill_in('Number of Employees', with: '12')
+    click_button('Create Item Shop')
+    save_and_open_page
+    expect(current_path).to eq('/item_shops')
+    expect(page).to have_content("Boomba's Brews")
+  end
+end
