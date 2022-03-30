b5d000b5f411a666a1a18598bfe378674e0cf9ae
Sherman Au <shermau13@gmail.com>
2022-03-30 13:27:56 -0600

    refactor:added back missing new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/item_shops', to: 'item_shops#index'
   get '/item_shops/new', to: 'item_shops#new'
-  post '/item_shops', to: 'item_shops#create'
   get '/item_shops/:id', to: 'item_shops#show'
   get '/item_shops/:item_shop_id/items', to: 'item_shop_items#index'
+  post '/item_shops', to: 'item_shops#create'
   get '/item_shops/:id/edit', to: 'item_shops#edit'
   patch '/item_shops/:id', to: 'item_shops#update'
 
