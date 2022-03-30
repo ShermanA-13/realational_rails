a9a911fcc95f559cd45e07d5ad4019d9708a76fc
Sherman Au <shermau13@gmail.com>
2022-03-30 14:14:19 -0600

    post path for item#create

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   get '/items', to: 'items#index'
   get '/items/new', to: 'items#new'
+  post '/items', to: 'items#create'
   get '/items/:id', to: 'items#show'
 end
