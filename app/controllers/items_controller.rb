2481d78007283892351a3cbbd32690c00fe230bb
Sherman Au <shermau13@gmail.com>
2022-03-30 14:16:03 -0600

    feat:#create for ItemController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,16 @@ class ItemsController < ApplicationController
   def show
     @items = Item.find(params[:id])
   end
+
+  def create
+    item = Item.create!(item_params)
+    item.save
+    redirect_to '/items'
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :quantity, :price, :consumable, :item_shop_id)
+  end
 end
