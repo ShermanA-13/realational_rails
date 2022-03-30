fbe0f55ee808fd039952b680c29cc7ef656a05a0
Sherman Au <shermau13@gmail.com>
2022-03-30 15:57:51 -0600

    refactor: strong params and parent/child corelation

--- app/controllers/item_shop_items_controller.rb
+++ app/controllers/item_shop_items_controller.rb
@@ -1,6 +1,6 @@
 class ItemShopItemsController < ApplicationController
   def index
-    item_shop = ItemShop.find(params[:item_shop_id])
-    @items = item_shop.items
+    @item_shop = ItemShop.find(params[:item_shop_id])
+    @items = @item_shop.items
   end
 end
