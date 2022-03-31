e363dd1b2c0d824c59b05bf3e55f8f986aedcee2
Sherman Au <shermau13@gmail.com>
2022-03-31 13:30:13 -0600

    feat:#sort_alpha used in params for ItemShopItemsContoller

--- app/controllers/item_shop_items_controller.rb
+++ app/controllers/item_shop_items_controller.rb
@@ -1,6 +1,10 @@
 class ItemShopItemsController < ApplicationController
   def index
     @item_shop = ItemShop.find(params[:item_shop_id])
-    @items = @item_shop.items
+    @items = if params[:sort]
+               @item_shop.sort_alpha
+             else
+               @item_shop.items
+             end
   end
 end
